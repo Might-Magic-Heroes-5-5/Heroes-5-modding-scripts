af92b47a5932cbe6d14bfc927ce3dd7be9a9e337
dredknight <gangstarrr@abv.bg>
2017-05-21 01:01:03 +0300

    minor changes

--- NCF_configuration_app/main.rb
+++ NCF_configuration_app/main.rb
@@ -12,17 +12,26 @@ Shoes.app(title: " v1.0", width: 500, height: 500, resizable: true ) do
 	end
 	
 	def list_creatures folder
-		@main.clear do
-			background beige
+		@main.clear fill: rgb(100,244,40) do
+			background beige#, width: @main.width, height: 2000
+			@from = 0
+			name_file = File.readlines("\NCF_lists\\legacy_list.txt")
+			#debug (File.readlines("\NCF_lists\\legacy_list.txt")[1..5])
 			(Dir.entries("NCF_repository\\Legacy pack").reject {|fn| fn == '.' or fn == '..'}).each_with_index do |ncf, i|
-				para "#{i+1}. #{ncf}"
+				num = ncf[/NCF_(.*?).pak/m, 1]
+				name_file[@from..-1].each do |line|
+					@from+=1
+					line.include?(num)? (para "#{num}. #{line.split(" ")[1]}";break;) : nil
+				end
 			end
 		end
+		debug("#{@main.width}, #{@main.height}")
+		#@main.contents[0].style width: @main.width, height: @main.height
 	end
 		
 	background tan
 	subtitle "NCF Configuration Utility", align: "center"
-	@main = stack left: 0.05, top: 0.15, width: 0.9, height: 0.8
+	@main = stack left: 0.05, top: 0.15, width: 0.9, height: 0.8, scroll: true
 	main_page
 		
 end
