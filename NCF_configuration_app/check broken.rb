ece88c31a967f471899fbc3e501601193f62e45e
dredknight <gangstarrr@abv.bg>
2017-05-23 23:24:31 +0300

    23052017 commit

--- NCF_configuration_app/check broken.rb
+++
@@ -1,7 +0,0 @@
-Shoes.app do
-	asd=flow left: 50, top: 50 do
-		check checked: false
-		para "Broke the shoes :("
-	end
-	start {debug(asd.contents[0].checked?)}
-end
--- NCF_configuration_app/main.rb
+++ NCF_configuration_app/main.rb
@@ -6,38 +6,72 @@ Shoes.app(title: " v1.0", width: 500, height: 600, resizable: false ) do
 				rect(left: 0, top: 0, curve: 10,  width: 435, height: 98, fill: rgb(245,245,220))
 				caption "Select NCF package to install", align: "center"
 			end
-			stack left: 40, top: 130, width: 152, height: 252 do
-				rect(left: 0, top: 0, curve: 10,  width: 150, height: 250, fill: rgb(245,245,220))
-				caption "Core", align: "center"
-				Dir.entries("NCF_core").reject {|fn| fn.include?('.')}.each_with_index { | f, i | button("#{f}", left: 25, top: 35 + 40*i, width: 100) { list_core f } }
-			end
+			@core = stack left: 40, top: 130, width: 152, height: 252;
+			main_core_block
 			stack left: 250, top: 130, width: 152, height: 252 do
 				rect(left: 0, top: 0, curve: 10,  width: 150, height: 250, fill: rgb(245,245,220))
 				caption "NCF package", align: "center"
-				Dir.entries("NCF_repository").reject {|fn| fn.include?('.')}.each_with_index { | f, i | button("#{f}", left: 25, top: 35 + 40*i, width: 100) { list_creatures f } }
+				Dir.entries("NCF_repository/packs").reject {|rj| rj.include?('.')}.each_with_index { | f, i | button("#{f}", left: 25, top: 35 + 40*i, width: 100) { list_creatures f } }
 			end
 			#inscription "Note: Packages are not compatible.", top: 400
 		end
 	end
 	
-	def list_core folder
-	 
-	
+	def main_core_block
+		@core.clear do
+			rect(left: 0, top: 0, curve: 10,  width: 150, height: 250, fill: rgb(245,245,220))
+			caption "Core", align: "center"
+			Dir.entries("NCF_repository/core").reject {|rj| rj.include?('.')}.each_with_index do | f, i |
+				Dir.glob("NCF_repository/core/#{f}/**/*").reject{ |rjj| File.directory?(rjj) }.each do |fn|
+					Dir.glob("data/**/#{fn.split("/")[-1]}") == [] ? @flag=1 : @flag=0;
+				end
+				@flag == 0? (exist_core f; break) : nil
+				button("#{f}", left: 25, top: 35 + 40*i, width: 100) { deploy_core f }
+			end
+		end
 	end
+	
+	def exist_core f
+		@core.clear do
+			rect(left: 0, top: 0, curve: 10,  width: 150, height: 250, fill: rgb(240,240,20))
+			caption "Core", align: "center"
+			caption "#{f}\nis deployed", align: "center"
+			button("Uninstall", left: 25, top: 200, width: 100 ) { purge_core }
+		end
+	end
+	
+	def purge_core
+		if [ "yes", "y", "Y", "YES" ].include?(ask("This will purge previous NCF installations. Are you sure(Y/N)?")) then
+			Dir.glob("NCF_repository/core/**/*").reject{ |rj| File.directory?(rj) }.each do |fn|
+				file_name = fn.split("/")[3..-1].join("/")
+				File.delete(file_name) if File.exist?(file_name)
+			end
+			main_core_block
+		end
+	end
+	
+	def deploy_core folder
+		if [ "yes", "y", "Y", "YES" ].include?(ask("Are you sure you want to deploy NCF core for #{folder}(Y/N)?\n")) then
+			FileUtils.copy_entry "NCF_repository/core/#{folder}/", "."
+			main_core_block
+		end
+	end
+		
+		
 	
 	def list_creatures folder
 		@custom_ncf_package = []
 		@main.clear fill: rgb(100,244,40) do
 			rect(left: 0, top: 0, curve: 10,  width: @main.width-3, height: @main.scroll_height-60, fill: rgb(245,245,220))
 			@from = 0
-			name_file = File.readlines("\NCF_lists\\legacy_list.txt")
+			name_file = File.readlines("NCF_repository/lists/legacy_list.txt")
 			tagline "Legacy pack list", align: "center"
 			line 20, 35, 420, 35
 			check(left: 30, top: 40, checked: false) { |c| @main.contents[6].contents.each { |f| f.contents[0].checked = c.checked? ? true : false} }
 			caption "Select All", left: 50, top: 39
 			line 20, 70, 420, 70
 			@q = stack left: 20, top: 75, width: 420, height: 400, scroll: true do
-				(Dir.entries("NCF_repository\\Legacy pack").reject {|fn| fn == '.' or fn == '..'}).each_with_index do |ncf, i|
+				(Dir.entries("NCF_repository/packs/Legacy pack").reject {|rj| rj == '.' or rj == '..'}).each_with_index do |ncf, i|
 					num = ncf[/NCF_(.*?).pak/m, 1]
 					name_file[@from..-1].each do |line|
 						@from+=1
