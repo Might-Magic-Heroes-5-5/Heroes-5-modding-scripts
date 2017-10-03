0fb7846a28b3c4784c37e98c4c34c8e92cc26f9e
dredknight <gangstarrr@abv.bg>
2017-10-03 19:52:53 +0300

    updating git with some new stuff :)

---
+++ utility scripts/fix_towns.rb
@@ -0,0 +1,28 @@
+Shoes.app do
+	NCF_dirs = Dir.entries("NCF_separated/").reject {|fn| fn.include?('.')}
+	count = 0
+	
+	### change all creatures .xdb files town affiliation to no_town
+	
+	NCF_dirs.each do |dr|
+		file_name = "NCF_separated/#{dr}/GameMechanics/Creature/Creatures/Neutrals/Creature_#{dr}.xdb"
+		if File.file?(file_name) then
+			text = File.read(file_name)
+			new_text = text.gsub(/<CreatureTown>.*<\/CreatureTown>/, "<CreatureTown>TOWN_NO_TYPE</CreatureTown>")
+			File.open(file_name, "w") { |file| file.puts new_text }
+		else
+			debug("Could not find creature xdb for #{dr}")
+		end
+	end
+	
+	### check all files consistency
+	
+	NCF_dirs.each do |dr|
+		if text.include?("<CreatureTown>TOWN_NO_TYPE</CreatureTown>") then
+			count+=1
+		else
+			debug(dr)
+		end
+	end
+	para "COMPLETE! #{pocket}"
+end
--- utility scripts/get_creature_names.rb
+++ utility scripts/get_creature_names.rb
@@ -1,34 +1,41 @@
 Shoes.app do
 
-
-def read_skills text, int = 0, first = nil, second = nil
-	skills = []
-	if File.file?(text) == true 
-		File.read(text).each_line do |line|
-			is=line.chop
-			if first.nil? or second.nil? then
-				skills << is
-			elsif (is[/#{first}(.*?)#{second}/m, 1]).nil? == false then
-				skills << (is)[/#{first}(.*?)#{second}/m, 1];
+	def read_skills text, int = 0, first = nil, second = nil
+		skills = []
+		if File.file?(text) == true 
+			File.read(text).each_line do |line|
+				is=line#.chop
+				if first.nil? or second.nil? then
+					skills << is
+				elsif (is[/#{first}(.*?)#{second}/m, 1]).nil? == false then
+					skills << (is)[/#{first}(.*?)#{second}/m, 1];
+				end
+			end
+			case int
+			when 1 then skills.each_with_index { |n, i|	skills[i] = n.to_i } 
+			when 2 then skills.each_with_index { |n, i|	skills[i] = n.to_f }
 			end
 		end
-		case int
-		when 1 then skills.each_with_index { |n, i|	skills[i] = n.to_i } 
-		when 2 then skills.each_with_index { |n, i|	skills[i] = n.to_f }
-		end
+		return skills
 	end
-	return skills
-end
-
-
-	(Dir.entries("Raw").reject {|fn| fn == '.' or fn == '..'}).each_with_index do |ncf, i|
-		#debug("All_NCF_creatures\\#{ncf}\\Text\\Game\\Creatures")
-		#Dir.glob("All_NCF_creatures/#{ncf}/Text/Game/Creatures/**/*").reject {|fn| fn.include?('Desc') or fn.include?('desc') or File.directory?(fn)}.each do |name|
-		Dir.glob("Raw/#{ncf}/GameMechanics/Creature/Creatures/**/*").reject {|fn| fn.include?('Desc') or fn.include?('desc') or File.directory?(fn)}.each do |name|
-			debug("#{ncf} #{File.read(name)}")
-			#debug(read_skills name, 0, "<BaseCreature>", "</BaseCreature>")
-			#D:\mod workplace\NCF\Raw\180\GameMechanics\Creature\Creatures\Neutrals\thunderbird.xdb
-			#debug(name)
+	
+	### Get creature names (taken from creature text file) and NCF id (taken from the name of the folder it resides)
+	
+	stack do
+		NCF_dirs = Dir.entries("NCF_separated/").reject {|fn| fn.include?('.')}
+		File.open("list.txt", "w") do |file|
+			NCF_dirs.each do |dr|
+				file_name = "NCF_separated/#{dr}/GameMechanics/Creature/Creatures/Neutrals/Creature_#{dr}.xdb"
+				if File.file?(file_name) then
+					visuals = read_skills file_name, 0, "<Visual href=\"", "#xpointer"
+					creature_name = read_skills "NCF_separated/#{dr}#{visuals[0]}", 0, "CreatureNameFileRef href=\"", "\"/>"
+					name = read_skills "NCF_separated/#{dr}#{creature_name[0]}"
+					file.puts "#{dr} #{name[0]}"
+				else
+					para "#{dr}. #{file_name}"
+				end
+			end
 		end
+		para "Finished"
 	end
 end
