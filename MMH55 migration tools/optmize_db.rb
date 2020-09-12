fc52364865ab51fc9a0d81fd3b17b3cd73683d5f
dredknight <gangstarrr@abv.bg>
2020-09-12 12:00:23 +0300

    up

--- MMH55 migration tools/optmize_db.rb
+++ MMH55 migration tools/optmize_db.rb
@@ -21,13 +21,14 @@ DB.execute( "CREATE INDEX artifacts_by_knowledge ON artifacts (knowledge);" )
 DB.execute( "CREATE INDEX artifacts_by_morale ON artifacts (morale);" )
 DB.execute( "CREATE INDEX artifacts_by_luck ON artifacts (luck);" )
 DB.execute( "CREATE INDEX artifacts_by_set ON artifacts (art_set);" )
+DB.execute( "CREATE INDEX artifacts_by_cost ON artifacts (cost);" )
 
-CLASSES = [ "HERO_CLASS_KNIGHT","HERO_CLASS_RANGERA","HERO_CLASS_RANGER","HERO_CLASS_WIZARDA","HERO_CLASS_WIZARD","HERO_CLASS_DEMON_LORD","HERO_CLASS_NECROMANCER","HERO_CLASS_NECROMANCERA","HERO_CLASS_WARLOCK","HERO_CLASS_WARLOCKB","HERO_CLASS_SHAMAN","HERO_CLASS_RUNEMAGE","HERO_CLASS_RUNEMAGEA","HERO_CLASS_BARBARIAN","HERO_CLASS_WARDEN","HERO_CLASS_HERETIC","HERO_CLASS_REAVER","HERO_CLASS_FLAMEKEEPERA","HERO_CLASS_BEASTMASTER","HERO_CLASS_GUILDMASTER","HERO_CLASS_SEER","HERO_CLASS_ENCHANTER","HERO_CLASS_SORCERER","HERO_CLASS_ELEMENTALIST","HERO_CLASS_KNIGHT_RENEGADE","HERO_CLASS_BARBARIAN_KHAN","HERO_CLASS_BARBARIAN_VET" ]
+CLASSES = DB.execute( "SELECT name FROM sqlite_master WHERE name LIKE 'HERO_CLASS_%' AND type == 'table';" )
 
 CLASSES.each do |c|
-	DB.execute( "CREATE INDEX '#{c}_by_type' ON #{c} (type, sequence);" )
-	DB.execute( "CREATE INDEX '#{c}_by_chance' ON #{c} (chance);" )
-	DB.execute( "CREATE INDEX '#{c}_by_skill' ON #{c} (skill);" )
+	DB.execute( "CREATE INDEX '#{c[0]}_by_type' ON #{c[0]} (type, sequence);" )
+	DB.execute( "CREATE INDEX '#{c[0]}_by_chance' ON #{c[0]} (chance);" )
+	DB.execute( "CREATE INDEX '#{c[0]}_by_skill' ON #{c[0]} (skill);" )
 end
 
 Shoes.app do
